495c9ee9d3ee6efc71782044f13134d1aa0a653b
gobanoff <a.kliosovas@yahoo.com>
2025-03-11 13:48:19 +0200

    s

--- test.py
+++ test.py
@@ -6,7 +6,50 @@ while a <=200:
 
 """
 
+"""
 x = 25
 while x >= 0:
     print(x)
     x -= 5
+"""
+
+"abc".upper()
+"abc".strip()
+"abcaaaaa".count("a")
+"""
+100/3=33.333333333333336
+100//3=33
+100%2=0
+100%3=1
+50 + 10 * 2 = 70
+(50 + 10) * 2 = 120
+
+d=250
+start='Monday'
+end='Friday'
+start + '-' + end= Monday-Friday
+
+r = input('4')
+
+name = input("Введите ваше имя: ")  
+print("Привет,", name)
+
+age = int(input("Введите ваш возраст: "))  
+print("Через год вам будет", age + 1)
+
+x = 5
+if x > 2:
+    x = -3
+if x > 1:
+    x = 1
+else:
+    x = 3
+print(x)
+3
+
+a, b = map(int, input("Введите два числа через пробел: ").split())  
+print("Сумма:", a + b)
+
+"""
+
+print()
